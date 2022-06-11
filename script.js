81ba5647bce950b4001ac2d3507e9b3cc69e0e7a
Dane Dalton <danedalton@sbcglobal.net>
2022-06-11 15:54:41 -0500

    Add an event listener for each button in the calculator to adjust display and get an operand

--- script.js
+++ script.js
@@ -1,18 +1,75 @@
 const display = document.querySelector('.display-text')
-const numButtons = document.querySelectorAll('.num')
 
+let operand = ''
 let prevOperand = ''
 let currOperand = display
 
-getNumberPress()
+buttonPresses()
 
 
+function buttonPresses() {
+    getNumberPress()
+    backSpaceNum()
+    allClear()
+    positiveNegative()
+    getOperand()
+}
+
+function getOperand() {
+    const operandButtons = document.querySelectorAll('.operand')
+
+    operandButtons.forEach(btn => {
+        btn.addEventListener('click', (e) => {
+            operand = ''
+            operand = e.target.textContent
+            console.log(operand)
+        })
+    })
+}
+
+//Allows for pos/neg change in current value
+function positiveNegative() {
+    const posNeg = document.querySelector('.pos-neg')
+
+    posNeg.addEventListener('click', () => {
+        if (display.textContent.includes('-')) return display.textContent = display.textContent.slice(1)
+
+        display.textContent = '-' + display.textContent
+    })
+}
+
+//Clears currentOp, prevOp, and operand. Displays 0
+function allClear() {
+    const acBtn = document.querySelector('.ac')
+
+    acBtn.addEventListener('click', () => {
+        display.textContent = '0'
+        prevOperand = ''
+        currOperand = display.textContent
+    })
+}
+
+//Deletes last integer in the number, leaves 0 as place holder if empty
+function backSpaceNum() {
+    const deleteBtn = document.querySelector('.del')
+    
+    deleteBtn.addEventListener('click', () => {
+        display.textContent = display.textContent.slice(0, -1)
+        if (display.textContent == '') display.textContent = '0'
+    }) 
+    
+}
 
 //Number buttons go to the display on click
 function getNumberPress() {
+    const numButtons = document.querySelectorAll('.num')
+
     numButtons.forEach(btn => {
         btn.addEventListener('click', (e) => {
             if (display.textContent.includes('.') && e.target.textContent.includes('.')) return
+
+            //Replaces the placeholder value
+            if (display.textContent == '0') return display.textContent = e.target.textContent
 
             display.textContent += e.target.textContent
         })
